bfdb86694b91acd4a4566f34540473d89d52b4e2
xwh6381518 <chrismooth90@hotmail.com>
2023-03-24 08:50:21 -0400

    Wenhao's work

--- tracker.py
+++ tracker.py
@@ -1,5 +1,8 @@
 '''
-Create a file tracker.py which offers the user the following options and makes calls to the Transaction class to update the database.
+tracker is an app that maintains a financial transactions list
+just as with the transactions code in this folder.
+
+it offers the user the following options and makes calls to the Transaction class to update the database.
 
 0. quit
 1. show categories
@@ -14,5 +17,95 @@ Create a file tracker.py which offers the user the following options and makes c
 10. summarize transactions by category
 11. print this menu
 
-The tracker.py program should not have any SQL calls and should be similar is structure to the todo2.py program from Lesson19
+but it also uses an Object Relational Mapping (ORM)
+to abstract out the database operations from the
+UI/UX code.
+
+The ORM, Transaction, will map SQL rows with the schema
+    (rowid,amount,date,desc)
+to Python Dictionaries as follows:
+
+(7,'1000','2020-01-01','rent payment') <-->
+
+{rowid:7,
+ amount:'1000',
+ date: '2020-01-01'
+ desc:'rent payment',
+ }
+
+In place of SQL queries, we will have method calls.
+
+This app will store the data in a SQLite database ~/tracker.db
+
+Recall that sys.argv is a list of strings capturing the
+command line invocation of this program
+sys.argv[0] is the name of the script invoked from the shell
+sys.argv[1:] is the rest of the arguments (after arg expansion!)
+
+Note the actual implementation of the ORM is hidden and so it 
+could be replaced with PostgreSQL or Pandas or straight python lists
+
 '''
+
+
+# here are some helper functions ...
+
+
+from transactions import Transaction
+import sys
+
+
+def process_args(arglist):
+    ''' examine args and make appropriate calls to Transaction'''
+    translist = Transaction()
+    if arglist == []:
+        print_usage()
+    elif arglist[0] == "show":
+        print_trans(trans=translist.selectAll())
+    elif arglist[0] == "quit":
+        sys.exit()
+    elif arglist[0] == 'add':
+        if len(arglist) != 4:
+            print_usage()
+        else:
+            transaction = {
+                'amount': arglist[1], 'date': arglist[2], 'desc': arglist[3]}
+            translist.add(transaction)
+    elif arglist[0] == 'delete':
+        if len(arglist) != 2:
+            print_usage()
+        else:
+            translist.delete(arglist[1])
+    elif arglist[0] == 'sum_day':
+        print_trans(trans=translist.selectDay())
+    elif arglist[0] == 'sum_month':
+        print_trans(trans=translist.selectMonth())
+    elif arglist[0] == 'sum_year':
+        print_trans(trans=translist.selectYear())
+    else:
+        print(arglist, "is not implemented")
+        print_usage()
+
+
+def toplevel():
+    ''' read the command args and process them'''
+    if len(sys.argv) == 1:
+        # they didn't pass any arguments,
+        # so prompt for them in a loop
+        print_usage()
+        args = []
+        while args != ['']:
+            args = input("command> ").split(' ')
+            if args[0] == 'add':
+                # join everyting after the name as a string
+                args = ['add', args[1], args[2], " ".join(args[3:])]
+            process_args(args)
+            print('-'*40+'\n'*3)
+    else:
+        # read the args and process them
+        args = sys.argv[1:]
+        process_args(args)
+        print('-'*40+'\n'*3)
+
+
+toplevel()
--- transactions.py
+++ transactions.py
@@ -1,14 +1,44 @@
 '''
-create a Python class Transaction in a new file transaction.py which will store financial transactions with the fields. 
-It should have an __init__ method where you pass in the filename for the database to be used (e.g. tracker.db) 
-and each transaction should have the following fields stored in a SQL table called transactions.
 
-'item #',
-'amount',
-'category',
-'date',
-'description'
+transaction.py is an Object Relational Mapping to the tracker database
 
-It should be similar to the Todolist ORM from Lesson 19 in class. It will allow the user to read and update the database as need.
-The transaction class should not do any printing!! 
+The ORM will work map SQL rows with the schema
+    (rowid,amount,date,desc)
+to Python Dictionaries as follows:
+
+(7,1000,'2020-01-01','rent payment') <-->
+{rowid:7,amount:1000,date:'2023-01-31',desc:'rent payment')
+
+In place of SQL queries, we will have method calls.
+
+This app will store the data in a SQLite database ~/tracker.db
+
 '''
+
+
+import sqlite3
+import os
+
+
+'''the Transaction class'''
+
+
+class Transaction():
+
+    def selectYear(self):
+        ''' return all of the completed tasks as a list of dicts.'''
+        return self.runQuery("SELECT rowid, SUM(amount), SUBSTRING(date, 1, 4), desc from tran GROUP BY SUBSTRING(date, 1, 4)", ())
+
+    def selectAll(self):
+        ''' return all of the transactions as a list of dicts.'''
+        return self.runQuery("SELECT rowid,* FROM tran", ())
+
+    def runQuery(self, query, tuple):
+        ''' return all of the financial transactions as a list of dicts.'''
+        con = sqlite3.connect('tracker.db')
+        cur = con.cursor()
+        cur.execute(query, tuple)
+        tuples = cur.fetchall()
+        con.commit()
+        con.close()
+        return [toDict(t) for t in tuples]
