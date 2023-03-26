898b0e998d49de0aeda4cbd09d22dba93fb0683b
ZH-project <zhihanli@brandeis.edu>
2023-03-26 17:19:33 -0400

    finished pytest. By Zhihan Li

--- test_transaction.py
+++ test_transaction.py
@@ -7,8 +7,8 @@ from transaction import to_dict, Transaction,tuples_to_dicts
 def tuples():
     " create some tuples to put in the database "
     return [("100", "type1", "2020-01-01", "test1"), 
-            ("200", "type2", "2020-01-02", "test2"),
-            ("300", "type1", "2020-02-01", "test3")
+            ("200", "type2", "2020-01-01", "test2"),
+            ("300", "type1", "2020-02-01", "test3"),
            ]
 
 # Zhihan Li
@@ -51,4 +51,72 @@ def test_select_all(trans, returned_dicts):
     results = ts.select_all()
     expected = returned_dicts
     assert results == expected
+
+# Zhihan Li
+def test_select_day(trans, returned_dicts):
+    ts = trans
+    results = ts.select_day()
+    list_ = []
+    expected = []
+    rid = 1
+    for d in returned_dicts:
+        sum = 0
+        temp = {}
+        if d['date'] not in list_:
+            list_.append(d['date'])
+            sum = int(d['amount'])
+            temp['rowid'] = rid
+            rid += 1
+            temp['amount'] = sum
+            temp['category'] = d['category']
+            temp['date'] = d['date']
+            temp['desc'] = d['desc']
+            expected.append(temp) 
+        else:
+            for i in range(len(list_)):
+                if expected[i]['date'] == d['date']:
+                    sum = int(d['amount']) + int(expected[i]['amount'])
+                    #returned_dicts[i]['amount'] = sum
+                    expected[i]['amount'] = sum
+                    rid += 1
+
+    assert results == expected
+
+# Zhihan Li
+def test_select_category(trans, returned_dicts):
+    ts = trans
+    results = ts.select_category()
+    list_ = []
+    expected = []
+    rid = 1
+    for d in returned_dicts:
+        sum = 0
+        temp = {}
+        if d['category'] not in list_:
+            list_.append(d['category'])
+            sum = int(d['amount'])
+            temp['rowid'] = rid
+            rid += 1
+            temp['amount'] = sum
+            temp['category'] = d['category']
+            temp['date'] = d['date']
+            temp['desc'] = d['desc']
+            expected.append(temp) 
+        else:
+            for i in range(len(list_)):
+                if expected[i]['category'] == d['category']:
+                    sum = int(d['amount']) + int(expected[i]['amount'])
+                    expected[i]['amount'] = sum
+                    rid += 1
+                    
+    assert results == expected
+
+# Zhihan Li
+def test_delete(trans, returned_dicts):
+    ts = trans
+    ts.delete(1)
+    results = ts.select_all()
+    expected = returned_dicts
+    assert results == expected[1:]
+
 
